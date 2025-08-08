8f62c53852f8a988c4af87c8a8e13b98766ed2f2
Pranay0412 <jainpranaytaral@gmail.com>
2025-08-08 10:13:23 +0530

    Add files via upload

---
+++ smartcityhub.sql
@@ -0,0 +1,518 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 08, 2025 at 06:24 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `smartcityhub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `Id` int(11) NOT NULL,
+  `UserName` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `area`
+--
+
+CREATE TABLE `area` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(155) NOT NULL,
+  `Latitude` double NOT NULL,
+  `Longitude` double NOT NULL,
+  `IsEmergencyPoint` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bus`
+--
+
+CREATE TABLE `bus` (
+  `Id` int(11) NOT NULL,
+  `LicensePlate` varchar(155) NOT NULL,
+  `Capacity` int(11) NOT NULL,
+  `CurrentRouteId` int(11) NOT NULL,
+  `CurrentAreaId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `Id` int(11) NOT NULL,
+  `Department` varchar(150) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `Issue` text NOT NULL,
+  `Status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `Id` int(11) NOT NULL,
+  `UserName` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `FullName` varchar(100) NOT NULL,
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emergencyservice`
+--
+
+CREATE TABLE `emergencyservice` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Type` varchar(100) NOT NULL,
+  `AreaId` int(11) NOT NULL,
+  `ContactNumber` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `Id` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `PlaceId` int(11) NOT NULL,
+  `Comments` text NOT NULL,
+  `Rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `metro`
+--
+
+CREATE TABLE `metro` (
+  `Id` int(11) NOT NULL,
+  `TrainName` varchar(100) NOT NULL,
+  `Capacity` int(11) NOT NULL,
+  `CurrentRouteId` int(11) NOT NULL,
+  `CurrentAreaId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parkinglot`
+--
+
+CREATE TABLE `parkinglot` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `AreaId` int(11) NOT NULL,
+  `Capacity` int(11) NOT NULL,
+  `CurrentOccupancy` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `route`
+--
+
+CREATE TABLE `route` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(155) NOT NULL,
+  `Length` double NOT NULL,
+  `IsBusRoute` tinyint(1) NOT NULL,
+  `IsMetroRoute` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `Id` int(11) NOT NULL,
+  `RouteId` int(11) NOT NULL,
+  `DepartureTime` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `station`
+--
+
+CREATE TABLE `station` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `AreaId` int(11) NOT NULL,
+  `IsBusStation` tinyint(1) NOT NULL,
+  `IsMetroStation` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `street`
+--
+
+CREATE TABLE `street` (
+  `Id` int(11) NOT NULL,
+  `StartAreaId` int(11) NOT NULL,
+  `EndAreaId` int(11) NOT NULL,
+  `Distance` double NOT NULL,
+  `IsOneWay` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `Id` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `RouteId` int(11) NOT NULL,
+  `IsBusTransport` tinyint(1) NOT NULL,
+  `IsMetroTransport` tinyint(1) NOT NULL,
+  `Time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `TotalBill` double NOT NULL,
+  `Distance` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `touristplace`
+--
+
+CREATE TABLE `touristplace` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(155) NOT NULL,
+  `AreaId` int(11) NOT NULL,
+  `Category` varchar(155) NOT NULL,
+  `Ratings` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `area`
+--
+ALTER TABLE `area`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `bus`
+--
+ALTER TABLE `bus`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `BusArea` (`CurrentAreaId`),
+  ADD KEY `BusRoute` (`CurrentRouteId`);
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `UserInfo` (`UserId`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `emergencyservice`
+--
+ALTER TABLE `emergencyservice`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `ServiceArea` (`AreaId`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `PlaceInfo` (`PlaceId`),
+  ADD KEY `CustomerInfo` (`UserID`);
+
+--
+-- Indexes for table `metro`
+--
+ALTER TABLE `metro`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `MetroArea` (`CurrentAreaId`),
+  ADD KEY `MetroRoute` (`CurrentRouteId`);
+
+--
+-- Indexes for table `parkinglot`
+--
+ALTER TABLE `parkinglot`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `LotArea` (`AreaId`);
+
+--
+-- Indexes for table `route`
+--
+ALTER TABLE `route`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `RouteInfo` (`RouteId`);
+
+--
+-- Indexes for table `station`
+--
+ALTER TABLE `station`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `StationArea` (`AreaId`);
+
+--
+-- Indexes for table `street`
+--
+ALTER TABLE `street`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `StartArea` (`StartAreaId`),
+  ADD KEY `EndArea` (`EndAreaId`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `CustomerTicket` (`UserID`),
+  ADD KEY `TravelRoute` (`RouteId`);
+
+--
+-- Indexes for table `touristplace`
+--
+ALTER TABLE `touristplace`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `PlaceArea` (`AreaId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `area`
+--
+ALTER TABLE `area`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `bus`
+--
+ALTER TABLE `bus`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `emergencyservice`
+--
+ALTER TABLE `emergencyservice`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `metro`
+--
+ALTER TABLE `metro`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `parkinglot`
+--
+ALTER TABLE `parkinglot`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `route`
+--
+ALTER TABLE `route`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `station`
+--
+ALTER TABLE `station`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `street`
+--
+ALTER TABLE `street`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `touristplace`
+--
+ALTER TABLE `touristplace`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bus`
+--
+ALTER TABLE `bus`
+  ADD CONSTRAINT `BusArea` FOREIGN KEY (`CurrentAreaId`) REFERENCES `area` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `BusRoute` FOREIGN KEY (`CurrentRouteId`) REFERENCES `route` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD CONSTRAINT `UserInfo` FOREIGN KEY (`UserId`) REFERENCES `customer` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `emergencyservice`
+--
+ALTER TABLE `emergencyservice`
+  ADD CONSTRAINT `ServiceArea` FOREIGN KEY (`AreaId`) REFERENCES `area` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `CustomerInfo` FOREIGN KEY (`UserID`) REFERENCES `customer` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `PlaceInfo` FOREIGN KEY (`PlaceId`) REFERENCES `touristplace` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `metro`
+--
+ALTER TABLE `metro`
+  ADD CONSTRAINT `MetroArea` FOREIGN KEY (`CurrentAreaId`) REFERENCES `area` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `MetroRoute` FOREIGN KEY (`CurrentRouteId`) REFERENCES `route` (`Id`);
+
+--
+-- Constraints for table `parkinglot`
+--
+ALTER TABLE `parkinglot`
+  ADD CONSTRAINT `LotArea` FOREIGN KEY (`AreaId`) REFERENCES `area` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `RouteInfo` FOREIGN KEY (`RouteId`) REFERENCES `route` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `station`
+--
+ALTER TABLE `station`
+  ADD CONSTRAINT `StationArea` FOREIGN KEY (`AreaId`) REFERENCES `area` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `street`
+--
+ALTER TABLE `street`
+  ADD CONSTRAINT `EndArea` FOREIGN KEY (`EndAreaId`) REFERENCES `area` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `StartArea` FOREIGN KEY (`StartAreaId`) REFERENCES `area` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `CustomerTicket` FOREIGN KEY (`UserID`) REFERENCES `customer` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `TravelRoute` FOREIGN KEY (`RouteId`) REFERENCES `route` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `touristplace`
+--
+ALTER TABLE `touristplace`
+  ADD CONSTRAINT `PlaceArea` FOREIGN KEY (`AreaId`) REFERENCES `area` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
