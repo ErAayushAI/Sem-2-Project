27ed8899d39a1e26a9e1d93cdb993bba6fb4b40a
PRANAY JAIN <jainpranaytaral@gmail.com>
2025-08-10 13:32:51 +0530

    init commit

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,23 +1,20 @@
 package Main;
 
 import Authentication.Login;
-import DataBase.DataBaseManager;
 
 import java.sql.SQLException;
-import java.util.Scanner;
+
 
 public class main {
     public static void main(String[] args) throws SQLException {
         Login l = new Login();
-        Scanner sc = new Scanner(System.in);
-        DataBaseManager con1= (DataBaseManager) DataBaseManager.getConnection();
-        do{
+        do {
             System.out.println("-------------------------------------------------");
             System.out.println("          Welcome to Smart city hub              ");
             System.out.println("-------------------------------------------------");
             System.out.println();
             l.loginMenu();
-        }while(true);
+        } while (true);
     }
 }
 
